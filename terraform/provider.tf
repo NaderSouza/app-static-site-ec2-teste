558a83afb1af8a3073c2f98387feb2653530ece0
Nader Hadad Souza <naderhadadsouza@gmail.com>
2023-10-04 17:48:56 -0300

    pipoca

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -11,9 +11,9 @@ terraform {
   }
 
     backend "s3" {
-      bucket         = "app-static-site-ec2-tf-timao"
+      bucket         = "app-static-site-ec2-tf-nadin"
       key            = "terraform.tfstate"
-      dynamodb_table = "app-static-site-ec2-tf-timao"
+      dynamodb_table = "app-static-site-ec2-tf-nadin"
       region         = "us-east-1"
     }
 
